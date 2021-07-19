8410bad0799ab9fecc2fb598df32dccc27359e03
unknown <obiimanuelpranata@gmail.com>
2021-07-19 23:44:00 +0900

    perbaiki tampilan pembelian

--- resources/views/pengguna/pembelian.blade.php
+++ resources/views/pengguna/pembelian.blade.php
@@ -18,7 +18,7 @@
 <section class="ftco-section ftco-cart">
     <div class="container">
         <div class="row">
-            <div class="col-md-12 ftco-animate">
+            {{-- <div class="col-md-12 ftco-animate">
                 <div class="cart-list">
                     <table class="table">
                          <thead class="thead-primary">
@@ -77,8 +77,46 @@
                             @endforeach
                         </tbody>
                     </table>
+                    
+                </div>
+            </div> --}}
+            @foreach ($pembelian as $p)
+            <div class="col-lg-4 mb-2">
+                <div class="card">
+                    <img src="/foto_produk/{{$p->foto}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$p->nama_produk}}</h5>
+                        <p class="card-text">
+                            Tgl Pembelian : {{$p->tgl_pembelian}}
+                        </p>
+                        <p class="card-text">
+                            Jumlah : {{$p->jml_produk}}
+                        </p>
+                        <p class="card-text">
+                            Total : Rp. {{number_format($p->total)}}
+                        </p>
+                        @if ($p->status == 1)
+                                        <small class="text-danger">Produk ini telah dinilai</small>
+                                    @else   
+
+                                        @for ($i = 1; $i <= 5; $i++)
+
+                                            @php
+                                                $id = "document.getElementById("."'nilai-produk-"."$p->kd_pembelian-$i"."').submit();";
+                                            @endphp
+                                            <a href="nilaiproduk/{{$p->kd_pembelian}}/{{$i}}" onclick="event.preventDefault(); {{$id}}">
+                                                <span class="ion-ios-star-outline rate-{{$i}} mr-2"></span>
+                                            </a>
+
+                                            <form id="nilai-produk-{{$p->kd_pembelian}}-{{$i}}" action="nilaiproduk/{{$p->kd_pembelian}}/{{$i}}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>                                                                    
+                                        @endfor
+                                    @endif
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 </section>
